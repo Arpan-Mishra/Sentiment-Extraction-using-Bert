e44104e772b689ec749a8e21f57384fcf11c7da1
Ghovaat <mishraarpan6@gmail.com>
2020-06-21 14:23:16 +0530

    Fixed Config added result

--- config.py
+++ config.py
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
-import tokenizers
 
-MAX_LEN = 100
-TRAIN_BATCH_SIZE = 16 
-VALID_BATCH_SIZE = 10 
-EPOCHS = 5
-BERT_PATH = 'bert-base-uncased'
-MODEL_PATH = 'model2.bin'
-TRAINING_FILE = 'C:/Users/Arpan/Downloads/Education/Project/Twitter Sentiment Classification/Data/train.csv'
-TOKENIZER = tokenizers.BertWordPieceTokenizer(
-        BERT_PATH+'-vocab.txt'        
-        )
-=======
 import tokenizers
 import transformers
 
@@ -22,11 +9,10 @@ TEST_BATCH_SIZE = 5
 EPOCHS = 5
 BERT_PATH = 'bert-base-uncased'
 MODEL_PATH = 'extraction_model.bin'
-TRAINING_FILE = 'C:/Users/Arpan/Downloads/Education/Project/Twitter Sentiment Classification/Data/train.csv'
-TESTING_FILE = 'C:/Users/Arpan/Downloads/Education/Project/Twitter Sentiment Classification/Data/test.csv'
-SENTIMENT_MODEL_PATH = 'C:/Users/Arpan/Downloads/Education/Project/Twitter Sentiment Classification/Text - Extraction/Sentiment Classifier/polarity_model.bin'
+TRAINING_FILE = 'C:/Users/Arpan/Downloads/Education/Project/Sentiment-Extraction-using-Bert/Data/train.csv'
+TESTING_FILE = 'C:/Users/Arpan/Downloads/Education/Project/Sentiment-Extraction-using-Bert/Data/test.csv'
+SENTIMENT_MODEL_PATH = 'C:/Users/Arpan/Downloads/Education/Project/Sentiment-Extraction-using-Bert/Sentiment Classifier/polarity_model.bin'
 TOKENIZER = tokenizers.BertWordPieceTokenizer(
         BERT_PATH+'-vocab.txt', lowercase=True        
         )
 Plrty_Tokenizer = transformers.BertTokenizer.from_pretrained(BERT_PATH,do_lower_case=True)
->>>>>>> 234f14c... Added app.py + final model
