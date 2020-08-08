2650cb1ab6a867ea8da25ebfb8b6d6e181c79bf2
Arpan Mishra <mishraarpan6@gmail.com>
2020-08-08 15:44:39 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-import config
-import transformers
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-class BertBaseUncased(nn.Module):
-    def __init__(self):
-        super(BertBaseUncased,self).__init__()
-        c = transformers.BertConfig.from_pretrained(config.BERT_PATH,output_hidden_states = True)                 
-        self.bert = transformers.BertModel.from_pretrained(config.BERT_PATH,config = c)
-        self.drop = nn.Dropout(0.1)
-        self.linear = nn.Linear(768,100)
-        self.out = nn.Linear(100,2)
-
-    def forward(self, ids,mask,token_type_ids):
-        _,_,o = self.bert(ids,
-                              mask,
-                              token_type_ids)
-        
-        # bs*max_len*768 + bs*max_len*768 = bs*max_len*(768*2)
-        out = torch.stack(o[-4:],dim=-1).sum(dim=-1)        
-        
-        out = F.relu(self.linear(out))
-        out = self.drop(out)
-        
-        out = self.out(out)
-        #bs*maxlen*1, bs*maxlen*1
-        start_logits, end_logits = out.split(1,dim=-1)
-        
-        start_logits = start_logits.squeeze(-1) # bs*maxlen
-        end_logits = end_logits.squeeze(-1) # bs*maxlen
- 
-        return start_logits,end_logits
-    
-    
-=======
 import config
 import transformers
 import torch
@@ -103,4 +65,3 @@ class Sentiment(nn.Module):
        # output = self.out(o)
         
         return o
->>>>>>> 234f14c... Added app.py + final model
